6668fd22dc2520478a70577a5bd3ef4c6468d1a5
Jerry Nwadinobi <110991183+fernandojerry@users.noreply.github.com>
2022-09-08 17:13:42 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * Main - prints a string
+ * Main - collect a string
+ * Description: prints with proper grammar, but the outcome is a piece of art,
  * Return: 0
  */
 int main(void)
