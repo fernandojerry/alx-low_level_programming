cd2d5e169e7307f48fe29e04d3fb2feb68a82290
Jerry Nwadinobi <fernandojerry19911500@gmail.com>
2022-09-08 08:37:00 -0700

    low level programing tasks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main - A string
+ * Description: prints "Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s", "Programming is like building a multilingual puzzle,\n");
+	return (0);
+}	
