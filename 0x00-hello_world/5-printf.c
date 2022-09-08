6715c58542afc87377c6b2780b2827cfdba97b3b
Jerry Nwadinobi <110991183+fernandojerry@users.noreply.github.com>
2022-09-08 17:06:17 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * Main - A string
- * Description: prints "with proper grammar, but the outcome is a piece of art,"
+ * Main - prints a string
  * Return: 0
  */
 int main(void)
