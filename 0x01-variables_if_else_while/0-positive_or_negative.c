2d6861b1e5427ca5321c690addd3b51d70e5c88a
Jerry Nwadinobi <110991183+fernandojerry@users.noreply.github.com>
2022-09-09 16:46:48 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <stdio.h> 
+#include <time.h> 
 /**
  * main - Entry point
  * Description: print whether the number stored in the variable n is positive
