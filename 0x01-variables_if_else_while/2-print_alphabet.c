4c0277e58b10b76f71401cf0c0889110898cb8db
Jerry Nwadinobi <fernandojerry19911500@gmail.com>
2022-09-09 11:41:44 -0700

    if, else and while condition

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: Print alphabets
+ * Return: 0
+ */
+int main(void)
+
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
