6693b01db3207b894f097b8ac5a67a96f834fcf2
Jerry Nwadinobi <fernandojerry19911500@gmail.com>
2022-09-13 06:21:22 -0700

    file ubdated

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: Prints 10 times the alphabet in lowercase
+ * Return: no return value
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	char j = 'a'
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 1; j <= 1; j++)
+		{
+			_putchar('j');
+		}
+		_putchar('i');
+	}
+	return (0);
+}
