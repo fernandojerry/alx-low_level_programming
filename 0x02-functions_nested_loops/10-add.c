23a04296bb0fd50c29f88c80916677d97226b3ea
Jerry Nwadinobi <fernandojerry19911500@gmail.com>
2022-09-13 21:21:44 -0700

    advaced tasks added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+/**
+ *
+ *  * add - adds two integers and returns the result
+ *
+ *   * @a: an integer
+ *
+ *    * @b: an integer
+ *
+ *     *
+ *
+ *      * Return: the sum of a and b
+ *
+ *       */
+
+int add(int a, int b)
+
+{
+
+		return (a + b);
+
+}
