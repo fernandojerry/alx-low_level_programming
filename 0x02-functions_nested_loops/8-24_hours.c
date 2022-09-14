52ad4c5adc49bc55c63c9182df48035c1426cc44
Jerry Nwadinobi <110991183+fernandojerry@users.noreply.github.com>
2022-09-14 04:51:00 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,10 +9,9 @@ void jack_bauer(void)
 	int hour = 0;
 	int minute = 0;
 
-	for (hour = 0; hour <= 24; hour++)
+	for (; hour < 24; hour++)
 	{
-		for (minute = 0; minute <= 60; minute++)
-			printf("%0d:%0d", hour, minute);
-		printf("\n");
+		for (minute = 0; minute < 60; minute++)
+			printf("%02d:%02d\n", hour, minute);
 	}
 }
