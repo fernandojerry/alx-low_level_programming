f26ff8aed7450966809c08c0e6f3fd1c863bc76b
Jerry Nwadinobi <fernandojerry19911500@gmail.com>
2022-10-06 04:50:41 -0700

    added more malloc and free tasks

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory usimg malloc.
+ * @b: memory allocated
+ * Return: a pointer to the allocated memory
+ * if malloc fails, the malloc_checked function should cause normal process
+ * termination with a status value of 98
+ */
+void *malloc_checked(unsigned in b)
+{
+	int *a;
+
+	a = malloc(b);
+	if (a == NULL)
+		exit(98);
+	return (a);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+more tasks on malloc and free
