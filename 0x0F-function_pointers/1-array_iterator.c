536a40c98fb62cae399f1a3ef5581533d1ad7115
Jerry Nwadinobi <110991183+fernandojerry@users.noreply.github.com>
2022-10-13 02:23:09 +0100

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,11 +13,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t i;
 if (array == NULL || action == NULL)
-prints an integer
-
-	return;
-
+{
+return;
+}
 for (i = 0; i < size; i++)
-	action(array[i]);
-
+{
+action(array[i]);
+}
 }
