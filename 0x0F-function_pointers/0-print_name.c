56784f3b840bd4e2833c9404870f178a47c17fff
Jerry Nwadinobi <fernandojerry19911500@gmail.com>
2022-10-12 18:05:19 -0700

    correction in )-print_name task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,20 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
  * print_name - Calls a function to print a name
  * @name: A pointer to a string
- * @f: A pointer to a function
+ * @f: A pointer 
+ * Return: nothing.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	void (*p)(char *);
-
 	if (name == NULL || f == NULL)
+	{
 		return;
-	p = f;
-	p(name);
+	}
+	else
+	{
+		f(name);
+	}
 }
