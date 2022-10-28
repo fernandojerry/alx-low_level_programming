2536f5475cb7a9a27f0255efabf3280b0be6e202
Jerry Nwadinobi <110991183+fernandojerry@users.noreply.github.com>
2022-10-28 23:06:02 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,31 +9,29 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len;
-	unsigned int i;
-	unsigned int j;
-	unsigned int sum;
-	unsigned int pow;
-	int base;
+	unsigned int ui;
+	int len, base_two;
 
-	base = 2;
-	sum = 0;
-	pow = 1;
-	if (b == NULL)
+	if (!b)
 		return (0);
-	for (len = 0; b[len] != '\0'; len++) /*Getting the length of b num*/
+
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
 		;
-	if (len == 1 && (b[0] == '0' || b[0] == '1')) /*Checking single nums*/
-		return ((b[0] - 48));
-	for (i = 0; b[i] != '\0'; i++) /*Iterating through the string*/
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
-		for (j = len - 1; j > 0; j--) /*Calculating the exponent*/
-			pow = pow * base;
-		sum = sum + (pow * (b[i] - 48)); /*Adding the number*/
-		len--;
-		pow = 1; /*Reseting the exponent*/
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-	return (sum);
+
+	return (ui);
 }
